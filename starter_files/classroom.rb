0de72c3d51e19a381642c466524cd7566df4e312
John Roche <roche.johnjr@gmail.com>
2017-08-16 21:57:47 -0400

    working with gavin's walkthrough

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -3,7 +3,14 @@
 # we are referring to them as 1-10.
 
 def assignment_score(grade_hash, student, assignment_num)
-  grade_hash.select { |name| name == student } [student] [assignment_num - 1]
+  # original
+  # grade_hash.select { |name| name == student } [student] [assignment_num - 1]
+  # p grade_hash
+  # p student
+  # p assignment_num
+  scores = grade_hash[student]
+  p scores
+  return scores[assignment_num - 1]
 end
 
 #  ^^^^ if argument equals a student's name, give me the grade and number of
@@ -18,27 +25,44 @@ end
 # "Give me the scores of assignment 5" ... for example
 
 def assignment_scores(grade_hash, assignment_num)
-  grade_hash.map { |scores| scores[1] [assignment_num - 1] }
+  # grade_hash.map { |scores| scores[1] [assignment_num - 1] }
+
+  # Call our existing method, the one we JUST wrote
+  # since it does the work we really need. That is, for
+  # this grade_hash and assignment_num AND the student
+  # we are currently iterating, give me that student's
+  # score for that assignment
+
+ grade_hash.map do |student, scores|
+   assignment_score(grade_hash, student, assignment_num)
+   # grade_hash.map { |student, scores| assignment_score(grade_hash, student, assignment_num) }
+
+ end
 end
 
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 def assignment_average_score(grade_hash, assignment_num)
-  total_scores = grade_hash.map { |scores| scores[1] [assignment_num - 1] }
-  total_scores.sum / grade_hash.length
+
+  # total_scores = grade_hash.map { |scores| scores[1] [assignment_num - 1] }
+  # total_scores.sum / grade_hash.length
+  scores = assignment_scores(grade_hash, assignment_num)
+  return scores.sum / scores.length
 end
 
 # Return a hash of students and their average score.
 # TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
 # use .to_h. Also look at Hash#transform_values.
 def averages(grade_hash)
-  assignments = grade_hash.take(1)[0][1].length
-  # returns 10
-  totals = grade_hash.transform_values { |grade| grade.sum }
-  # returns :nevada => 707
-  avg = totals.transform_values { |total| total / assignments}
-  # indiana => 81
+  # assignments = grade_hash.take(1)[0][1].length
+  # # returns 10
+  # totals = grade_hash.transform_values { |grade| grade.sum }
+  # # returns :nevada => 707
+  # avg = totals.transform_values { |total| total / assignments }
+  # # indiana => 81
+
+
 end
 
 # Return a letter grade for a numerical score.
@@ -64,7 +88,7 @@ end
 # Return a hash of students and their final letter grade, as determined
 # by their average.
 def final_letter_grades(grade_hash)
-
+  averages(grade_hash).transform_values { |average| letter_grade(average) }
 end
 
 # Return the average for the entire class.
